0ffc4c7415443e3372c8d6f17c0bf6f6c2d72fc4
Harry Candlish <hcnd90@gmail.com>
2022-11-22 10:42:57 +1300

    navbar completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,35 @@
-import React from 'react'
+import { useState } from 'react'
+
 import {Link, NavLink} from 'react-router-dom'
 import './navbar.css'
 import {links} from '../data'
 import {AiOutlineBars} from 'react-icons/ai'
+import {MdOutlineClose} from 'react-icons/md'
 import {ImBooks} from 'react-icons/im'
 
 const Navbar = () => {
+    const [isNavShowing, setIsNavShowing] = useState(false);
   return (
     <nav>
         <div className="container nav_container">
-            <Link to='/' className='logo' ><ImBooks/></Link>
-            <ul className='nav_links'>
+            <Link to='/' className='link' ><ImBooks className='logo'/></Link>
+            <ul className={`nav_links ${isNavShowing ? 'show_nav' : 'hide_Nav'}`}>
+                
                 {
                     links.map(({name, path}, index) => {
                         return (
-                            <NavLink to={path}>{name}</NavLink>
+                            <li key={index}>
+                            <NavLink to={path} className={({isActive}) => isActive ? 'active-nav' : ''}>{name}</NavLink>
+                            </li>
                         )
                     })
                 }
+                
             </ul>
-            <button className="nav_toggle-btn">
-                <AiOutlineBars/>
+            <button className="nav_toggle-btn" onClick={() => setIsNavShowing(!isNavShowing)}>
+                {
+                    isNavShowing ? <MdOutlineClose/> : <AiOutlineBars/>
+                }
             </button>
         </div>
     </nav>
--- src/data.js
+++ src/data.js
@@ -1,5 +1,9 @@
 export const links = [
     {
+        name: "Home",
+        path: '/#'
+    },
+    {
         name: "About",
         path: '/about'
     },
