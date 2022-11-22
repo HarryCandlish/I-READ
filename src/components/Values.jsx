49add1d67ba7cd3d8327caf12e80e3dfe7283ecd
Harry Candlish <hcnd90@gmail.com>
2022-11-22 23:01:44 +1300

    values section of home page complete and styled

---
+++ src/components/Values.jsx
@@ -0,0 +1,42 @@
+import Image from '../images/values.jpg'
+import SectionHead from './SectionHead'
+import {GiCutDiamond} from 'react-icons/gi'
+import {values} from '../data'
+import Card from '../UI/Card'
+
+const Values = () => {
+  return (
+    <section className="values">
+        <div className='container values_container'>
+            <div className="values_left">
+                <div className="values_image">
+                    <img src={Image} alt="value image"/>
+                </div>
+            </div>
+            <div className="values_right">
+                <div className="values_icon">
+                <span><GiCutDiamond/></span>
+                <h2>Values</h2>
+                </div>
+                <p>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum 
+                </p>
+                <div className="values_wrapper">
+                    {
+                        values.map(({id, icon, title, desc}) => {
+                            return <Card className='values_value'>
+                                <span>{icon}</span>
+                                <h4>{title}</h4>
+                                <small>{desc}</small>
+                                </Card>
+                        })
+                    }
+                </div>
+            </div>
+        </div>
+
+    </section>
+  )
+}
+
+export default Values
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './home.css'
 import MainHeader from '../../components/MainHeader'
 import Product from '../../components/Product'
+import Values from '../../components/Values'
 
 const Home = () => {
   return (
     <>
     <MainHeader/>
     <Product/>
+    <Values/>
     </>
   )
 }
