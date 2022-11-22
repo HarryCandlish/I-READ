d85f7632e4f3ce6e1d45ce0f05d4859fe2e79ae2
Harry Candlish <hcnd90@gmail.com>
2022-11-22 16:40:58 +1300

    header completed and styled for home page

---
+++ src/components/Product.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Product = () => {
+  return (
+    <section className="product">
+        <div className="container product_container">
+            
+        </div>
+    </section>
+  )
+}
+
+export default Product
